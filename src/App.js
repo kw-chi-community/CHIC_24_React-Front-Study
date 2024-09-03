e2707d7a805ade11bc8ba91c59465ce12bb1d430
GaYeon <yun02190219@gmail.com>
2024-09-04 03:37:57 +0900

    주택공급문제/환경문제/교통량문제 그래프 초안

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Traffic from './Component/Traffic';
+import House from './Component/House';
+import Environment from './Component/Environment';
+
+function App() {
+  return (
+    <div className="App">
+      <Traffic/>
+      <House />
+      <Environment />
+    </div>
+  );
+}
+
+export default App;
